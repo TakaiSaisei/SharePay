2a111960fb467ee09bf44a3a08790fe885601f00
Nikita Kolesnikov <takaisaisei@gmail.com>
2022-04-10 21:38:14 +0300

    Remove #owes from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,4 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :phone, presence: true, uniqueness: true
-
-  def owes(user)
-    Payment.where(sender: self, receiver: user).sum(:amount) -
-      UserPurchase.includes(:purchase).where('user_purchases.user': self, 'purchases.user': user).sum(:amount)
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,32 +16,4 @@ RSpec.describe User, type: :model do
   it 'has a valid factory' do
     expect(user).to be_valid
   end
-
-  describe '#owes' do
-    let_it_be(:receiver) { create :user }
-    let_it_be(:amount) { 100 }
-
-    context 'when there are no payments and purchases' do
-      it 'returns 0' do
-        expect(user.owes(receiver)).to eq 0
-      end
-    end
-
-    context 'when there are purchases and no payments' do
-      let!(:purchases) { create_list :purchase, 3, :with_items, user: receiver, items_user: user, items_amount: amount }
-
-      it 'returns sum of purchases' do
-        expect(user.owes(receiver)).to eq(-amount * 3)
-      end
-    end
-
-    context 'when there are purchases and payments' do
-      let!(:purchases) { create_list :purchase, 3, :with_items, user: receiver, items_user: user, items_amount: amount }
-      let!(:payment) { create_list :payment, 2, sender: user, amount:, receiver: }
-
-      it 'returns sum of purchases minus sum of payments' do
-        expect(user.owes(receiver)).to eq(-amount)
-      end
-    end
-  end
 end
