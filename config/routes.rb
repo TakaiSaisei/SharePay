7132df77e71f27d75d5c17bd88df041cedb6dee0
Nikita Kolesnikov <takaisaisei@gmail.com>
2022-04-26 21:25:19 +0300

    Drop users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :debts, only: %i[index show]
   resources :payments, only: %i[index create]
   resources :purchases, except: %i[update destroy]
-  resources :users, except: %i[create]
 
   namespace :auth do
     post :login
