d7560bfab4aac596148c586eefd6989c2c55d410
Nikita Kolesnikov <takaisaisei@gmail.com>
2022-04-27 21:23:50 +0300

    Fix payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -17,6 +17,10 @@ class PaymentsController < ApplicationController
   private
 
   def payment_params
+    if params[:payment][:receiver_phone].present?
+      params[:payment][:receiver_id] = User.find_by(phone: params[:payment][:receiver_phone]).id
+    end
+
     params.require(:payment).permit(:amount, :currency, :receiver_id).with_defaults(sender_id: current_user.id)
   end
 end
