0ff2129f4bb5b7424da04f4cc2dbafb318349e0a
Nikita Kolesnikov <takaisaisei@gmail.com>
2022-05-08 17:37:18 +0300

    Filter debts

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -3,7 +3,7 @@ class DebtsController < ApplicationController
 
   # GET /debts
   def index
-    @debts = current_user.mine_debts
+    @debts = current_user.mine_debts.where('amount != 0.0')
   end
 
   # GET /debts/{id}
